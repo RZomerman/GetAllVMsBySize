cc000b77417eb98a33de42e9261e3390ea2ef2f3
Roelf Zomerman <RZomerman@users.noreply.github.com>
2019-12-05 11:21:20 +0400

    Update GetVMSizes.ps1

--- GetVMSizes.ps1
+++ GetVMSizes.ps1
@@ -1,27 +1,5 @@
 
-Function Get-AzVMsBySize {
-    Param (
-        [parameter()]
-        $allSubscriptions
-    )   
-    $AllVMs=get-azvm
-    foreach ($VM in $AllVMs) {
-        $VMSizes=$null
-        $VMSizes = New-Object System.Collections.ArrayList
-        $supressdisplay=$VMSizes.Add($vm.HardwareProfile.VmSize)
-        If ($allSubscriptions) {
-            $supressdisplay=$AllSubscriptionVMs.Add($vm.HardwareProfile.VmSize)
-        }
-    }
 
-        If ($vm.count -gt 1) {
-            $UniqueSizes=$VMSizes | sort -Unique
-            Foreach ($UniqueSize in $UniqueSizes) {
-                $VMofSize=$AllVMs | where {$_.HardwareProfile.VmSize -eq $UniqueSize}
-                Write-Host ($UniqueSize +": " + $VMofSize.count)
-            }
-    }
-}
 
 
 $AllSubscriptionVMs = New-Object System.Collections.ArrayList
@@ -31,8 +9,30 @@ Foreach ($subscription in $Subscriptions) {
     write-host ""
     Write-host ("Subscription: " + $subscription.Name) -ForegroundColor Green
     $context=Set-AzContext -Subscription $Subscription.subscription
-    Get-AzVMsBySize -allSubscriptions $true
+    $VMSizes=$null
+    $VMSizes = New-Object System.Collections.ArrayList
+    $AllVMs=get-azvm
+    foreach ($VM in $AllVMs) {
+        $supressdisplay=$VMSizes.Add($vm.HardwareProfile.VmSize)
+        $supressdisplay=$AllSubscriptionVMs.Add($vm.HardwareProfile.VmSize)
+        
+    }
+
+        If ($AllVMs.count -ge 1) {
+            $UniqueSizes=$VMSizes | sort -Unique
+            Foreach ($UniqueSize in $UniqueSizes) {
+                $VMofSize=$AllVMs | where {$_.HardwareProfile.VmSize -eq $UniqueSize}
+                Write-Host ($UniqueSize +": " + $VMofSize.count)
+            }
+    }
+
 
     write-host "all sizes in all subsciptions:"
     $AllSubscriptionVMs | group | % { $h = @{} } { $h[$_.Name] = $_.Count } { $h }
 }
+
+
+
+
+
+
